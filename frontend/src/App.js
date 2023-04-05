90afd0ebc7d249b9016b2ad20394ee59435e01ed
Sebastian <bastianonofrei@gmail.com>
2023-04-06 01:10:31 +0300

    Created dummy home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
- import {Container} from 'react-bootstrap'
- import Header from "./components/Header";
- import Footer from "./components/Footer";
- 
+import {Container} from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from './screens/HomeScreen';
+
 function App() {
   return (
 		<>
 			<Header />
-			<main className='py-3'>
+			<main className="py-3">
 				<Container>
-					<h1>Welcome to Shop</h1>
+					<HomeScreen />
 				</Container>
 			</main>
 			<Footer />
 		</>
 	);
 }
+
 
 export default App;
