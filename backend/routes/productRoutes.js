eb67f970c61ca1d78ab3c02565cb87daaee54fba
Sebastian <bastianonofrei@gmail.com>
2023-04-18 16:02:28 +0300

    created create and update be product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -50,4 +50,57 @@ router.delete("/:id", asyncHandler(async(req, res) => {
     }
 }));
 
+
+//  @description Request pentru a crea un produs
+//  @route POST /api/products
+//  @access  Private/Admin
+router.post("/", asyncHandler(async(req, res) => {
+    const product = new Product({
+        name: 'Nume produs',
+        price: 0,
+        user : req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Nume Brand',
+        category: 'Nume categorie',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Descriere...'
+    })
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct)
+}));
+
+
+
+//  @description Request pentru a updata un produs
+//  @route PUT /api/products/:id
+//  @access  Private/Admin
+router.put("/:id", asyncHandler(async(req, res) => {
+   
+    const {name,price,description,image,brand,category,countInStock} = req.body
+
+    const product = await Product.findById(req.params.id)
+    if(product){
+
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+	res.json(updatedProduct);
+    }else{
+        res.status(404)
+        throw new Error('Product not found')
+    }
+  
+}));
+
+
+
+
 module.exports = router;
